684c30a6bd449c966505dbdabc04a390681c71c3
Ramy Phoenix <recoveryfreenethub@gmail.com>
2025-09-09 11:47:50 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-MONGO_URL = getenv("MONGO_URL")
+BOT_TOKEN = getenv("8321864901:AAEQeg7Ct9JyGDc8RDMA736rGESs886lck0")
+MONGO_URL = getenv("mongodb+srv://anonymousguywas:12345Trials@cluster0.t4nmrtp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-OWNER_ID = int(getenv("OWNER_ID", 1356469075))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FallenAssociation")
+OWNER_ID = int(getenv("OWNER_ID", 5962658076))
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Megahubbots")
+
